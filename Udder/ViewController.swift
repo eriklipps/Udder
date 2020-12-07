b3733cef4706bb3119919eb9834f6bef2d9fe243
Erik Lipps <eriklipps@gmail.com>
2020-12-07 09:53:17 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Erik Lipps on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
